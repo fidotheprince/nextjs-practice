c946b321d9a02710863970b71a1ea135eb0b51e0
Alvaro Castro-Cid <alcastrocid24@gmail.com>
2022-12-23 15:23:28 -0500

    intro to the use of Head component and routing;

---
+++ next-pract/pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About</title>
+                <meta name='keywords' content='web development, programming' />
+            </Head>
+            <meta name='keywords' content='web development, programming' />
+
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about;
