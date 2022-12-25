567c6572a6a552e6d89ed3d0204cd55b2692e6ff
Alvaro Castro-Cid <alcastrocid24@gmail.com>
2022-12-24 20:16:31 -0500

    added styles and practiced fetching from an external api;

--- next-pract/components/Header.js
+++ next-pract/components/Header.js
@@ -4,9 +4,12 @@ import headerStyles from '../styles/Header.module.css'
 export const Header = () => {
   return (
     <div>
-        <h1 className="title">
+        <h1 className={headerStyles.title}>
             <span> WebDev </span> News
         </h1>
+        <p className={headerStyles.description}>
+           Keep up to date with the latest news 
+        </p>
         {/* example of embedded styling */}
         {/* <style jsx>{`.title {color: red;}`}</style> */}
     </div>
