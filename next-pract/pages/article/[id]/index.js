6f00e778db9a1f097f25936a4c36e150d349c52e
Alvaro Castro-Cid <alcastrocid24@gmail.com>
2022-12-26 11:03:11 -0500

    Implemented single post fetching with async getServerSideProps function;

--- next-pract/pages/article/[id]/index.js
+++ next-pract/pages/article/[id]/index.js
@@ -1,9 +1,24 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-export default function article() {
-    const router = useRouter()
-    const { id } = router.query;
+export default function article({ article }) {
+    // const router = useRouter()
+    // const { id } = router.query;
+    // console.log(id);
   return (
-    <div>this is an articles id is {id}</div>
+    <>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br />
+        <Link href="/">Go Back</Link>
+    </>
   )
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+    return {
+        props: { article }
+    }
 }
