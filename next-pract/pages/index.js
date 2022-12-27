74df3e12a276a500f18ac083947fc27da69a4aac
Alvaro Castro-Cid <alcastrocid24@gmail.com>
2022-12-27 10:35:52 -0500

    Executing a request to our backend API;

--- next-pract/pages/index.js
+++ next-pract/pages/index.js
@@ -10,8 +10,10 @@ export default function Home({articles}) {
   )
 }
 
+/* Example of request to our backend API */
+
 export const getStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const res = await fetch('http://localhost:3000/api/articles')
   const articles = await res.json();
   /**
    * expose fetch data to component via
@@ -21,3 +23,17 @@ export const getStaticProps = async () => {
     props: { articles }
   }
 }
+
+/* Example of request to an external API */
+
+// export const getStaticProps = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+//   const articles = await res.json();
+//   /**
+//    * expose fetch data to component via
+//    * setting to props
+//    */
+//   return {
+//     props: { articles }
+//   }
+// }
