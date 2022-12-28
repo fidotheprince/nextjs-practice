1020bbcbb8c147514b023211536cb4706921434c
Alvaro Castro-Cid <alcastrocid24@gmail.com>
2022-12-28 10:24:42 -0500

    add global meta component;

--- next-pract/components/ArticleItem.js
+++ next-pract/components/ArticleItem.js
@@ -1,11 +1,13 @@
 import articleStyles from '../styles/Article.module.css'
 import Link from 'next/link'
+import Meta from './Meta'
 
 export default function ArticleItem({article}) {
     
   return (
     /*Demonstrates how to dynamically set a endpoint*/
     <Link href="/article/[id]" as={`/article/${article.id}`}>
+      <Meta title={article.title} />
         <div className={articleStyles.card}>
             <h2>{article.title} &rarr;</h2>
             <p>{article.body}</p>
--- next-pract/pages/index.js
+++ next-pract/pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
 
 export default function Home({articles}) {
